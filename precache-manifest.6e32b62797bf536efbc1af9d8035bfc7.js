b07a296d6792c2ab52bd5f065b1f22e0110d77bf
Dylan <d_h@mit.edu>
2020-06-12 08:57:05 -0400

    build 6/12

---
+++ precache-manifest.6e32b62797bf536efbc1af9d8035bfc7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adbc07b6be8df1440536c488b12616a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d788ac02aa87ca7392e8",
+    "url": "/static/css/main.eba85d16.chunk.css"
+  },
+  {
+    "revision": "b3f04c03cda390f34e74",
+    "url": "/static/js/2.d538e2a5.chunk.js"
+  },
+  {
+    "revision": "68ba00d5eb083746d913686923a3d904",
+    "url": "/static/js/2.d538e2a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d788ac02aa87ca7392e8",
+    "url": "/static/js/main.127a8878.chunk.js"
+  },
+  {
+    "revision": "09b1c3bc040b3a605d6f",
+    "url": "/static/js/runtime-main.1cf00c66.js"
+  },
+  {
+    "revision": "8dad80289a1ece380591ad143b616747",
+    "url": "/static/media/logo.8dad8028.svg"
+  }
+]);
